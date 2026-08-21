20b7612624561df474b77fc1877e1a57e0a3982e
Mahesh Kumar <maheshkumar64b@gmail.com>
2026-08-21 18:00:52 +0530

    Add all programme question of lec4 and make assignmentOne file

--- c/basic/Lec4Tasks.c
+++ c/basic/Lec4Tasks.c
@@ -23,8 +23,34 @@
 #include <stdio.h>
 
 int main (){
-
+/*
     int num1 = 722;
     printf("Value is : %d", num1);
+
+    float k = 2.5;
+    printf("%f\n", k);
+    printf("%.2f\n", k);
+    printf("%.80f", k);
+
+    double p = 2.729286169261855911915555819915;
+    printf("%.99lf",p);
+
+    int a = 5, b = 6;
+    printf("%d %d\n",a,b);
+    printf("a=%d and b=%d\n",a,b);
+    printf("a=%d & b=%d\n",b,a);
+    printf("Value of a is %d and value of b is %d\n",a,b);
+    printf("Sum of %d and %d is %d\n",a,b,a+b);
+    printf("%d + %d = %d",a,b,a+b);
+
+    int a,b;
+    printf("Enter two numbers : ");
+    scanf("%d %d", &a, &b);
+    printf("Sum of those numbers are %d", a+b); 
+
+*/
+
+    
+
     return 0;
 }
