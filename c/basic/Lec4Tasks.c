3e590d0e00a20605844852467952dac241b00fe3
Mahesh Kumar <maheshkumar64b@gmail.com>
2026-08-21 16:57:03 +0530

    remove task2 file rename task1 to Lec4Tasks and make a programme to create variable and print its value on screen

---
+++ c/basic/Lec4Tasks.c
@@ -0,0 +1,30 @@
+// Task 1
+
+// #include <stdio.h>
+
+// int main (){
+
+//     printf("Welcome!");
+//     return 0;
+// }
+
+// Task 2
+
+// #include <stdio.h>
+
+// int main () {
+
+//     printf("Welcome\nStudents");
+//     return 0;
+// }
+
+// Task 3
+
+#include <stdio.h>
+
+int main (){
+
+    int num1 = 722;
+    printf("Value is : %d", num1);
+    return 0;
+}
