82c656879eac591276244f96986b808c38e04127
Mahesh Kumar <maheshkumar64b@gmail.com>
2026-08-21 22:31:42 +0530

    assignment two all 10 question done

--- assignments/c/AssignmentTwo.c
+++ assignments/c/AssignmentTwo.c
@@ -1,29 +1,101 @@
 // Question 1 --------->
 
+// #include <stdio.h>
+
+// int main (){
+//     int a,b,c,d;
+//     scanf("%d %d %d",&a,&b,&c);
+//     d = (a+b+c)/3;
+//     printf("%d is avg",d);
+// }
 
 // Question 2--------->
 
+// #include <stdio.h>
+// int main (){
+//     double r,a;
+//     scanf("%lf",&r);
+//     a = ( 2 * r) * 3.14159;
+//     printf("%lf",a);
+// }
 
 // Question 3--------->
 
+// #include <stdio.h>
+// int main (){
+//     double p,r,t,si;
+//     scanf("%lf %lf %lf",&p,&r,&t);
+//     si = (p * r * t) / 100;
+//     printf("%lf",si);
+// }
 
 // Question 4--------->
+// #include <stdio.h>
+// int main (){
+//     int l,w,h,a;
+//     scanf("%d %d %d",&l,&w,&h);
+//     a = l * w * h;
+//     printf("%d",a);
+// }
 
 
 // Question 5--------->
+// #include <stdio.h>
+// int main (){
+//     double c,s,p;
+//     printf("Enter the cost price of 12 banana :");
+//     scanf("%lf",&c);
+//     printf("Enter the selling price of 12 banana :");
+//     scanf("%lf",&s);
+//     c = c / 12;
+//     s = s / 12;
+//     p = s - c;
+//     printf("Total profit for selling 1 banana is %lf and for 25 banana is %lf !",p,p*25);
+
+// }
 
 
 // Question 6--------->
 
+// #include <stdio.h>
+// int main (){
+//     char c;
+//     scanf("%c",&c);
+//     printf("%d",c);
+// }
 
 // Question 7--------->
 
+// #include <stdio.h>
+// int main (){
+//     int c;
+//     scanf("%d",&c);
+//     printf("%c",c);
+// }
 
 // Question 8--------->
 
+// #include <stdio.h>
+// int main (){
+//     char a,b,c;
+//     scanf("%c %c %c", &a,&b,&c);
+//     printf("%d=%c and %d=%c and %d=%c",a,a,b,b,c,c);
+// }
 
 // Question 9 ------->
 
+// #include <stdio.h>
+// int main (){
+//     int date,month,year;
+//     scanf("%d/%d/%d",&date,&month,&year);
+//     printf("Day - %d ,Month - %d ,Year - %d",date,month,year);
+// }
 
 // Question 10--------->
 
+#include <stdio.h>
+int main (){
+    int hh,mm;
+    scanf("%d:%d",&hh,&mm);
+    printf("%d hour and %d Minutes",hh,mm);
+}
