41923be0c3d70962e9af049f1badec4819521137
Mahesh Kumar <maheshkumar64b@gmail.com>
2026-08-21 18:50:04 +0530

    Assignment one all 10 question complete 10 programmes

---
+++ assignments/c/AssignmentOne.c
@@ -0,0 +1,107 @@
+// Question 1 --------->
+
+/*
+#include <stdio.h>
+
+int main () {
+    printf("Hello Students");
+    return 0;
+}
+*/
+
+// Question 2--------->
+/*
+#include <stdio.h>
+
+int main () {
+    printf("Hello\nStudents");
+    return 0;
+}
+*/
+// Question 3--------->
+
+// #include <stdio.h>
+
+// int main () {
+//    printf("\"MySirG\"");
+//     return 0;
+// }
+
+// Question 4--------->
+
+// #include <stdio.h>
+
+// int main () {
+//    printf("\\n");
+//     return 0;
+// }
+
+// Question 5--------->
+
+
+// #include <stdio.h>
+
+// int main () {
+//    printf("\\r");
+//     return 0;
+// }
+
+
+// Question 6--------->
+
+
+// #include <stdio.h>
+
+// int main () {
+//    printf("\"Teacher\'s Day\"");
+//     return 0;
+// }
+
+// Question 7--------->
+
+// #include <stdio.h>
+
+// int main (){
+//     int a,b,c;
+//     printf("Enter two numbers : ");
+//     scanf("%d %d", &a, &b);
+//     c = a + b;
+//     printf("Sum of %d and %d is %d",a,b,c);
+//     return 0;
+// }
+
+// Question 8--------->
+
+// #include <stdio.h>
+// int main(){
+//     int a,b;
+//     printf("Enter a number : ");
+//     scanf("%d", &a);
+//     b = a * a;
+//     printf("Square of number %d is %d",a,b);
+// }
+
+// Question 9 ------->
+
+// #include <stdio.h>
+// int main (){
+//     int l,w,a;
+//     printf("Enter Lenght and width of ractangle : ");
+//     scanf("%d %d", &l,&w);
+//     a = l * w;
+//     printf("%d is Area", a);
+// }
+
+// Question 10--------->
+
+
+// #include <stdio.h>
+
+// int main () {
+//     double r,a;
+//     printf("Enter radius of circle : ");
+//     scanf("%lf",&r);
+//     a =  3.14159265359*(r*r);
+//     printf("Area of circle is %lf having the radius %.2lf",a,r);
+//     return 0;
+// }
